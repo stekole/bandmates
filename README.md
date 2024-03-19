f8769e584b1bfdeb652386a985d88a5a8443b95d
stekole <30674956+stekole@users.noreply.github.com>
2024-03-19 00:54:24 -0400

    Create requirements.txt (#1)
    * Create requirements.txt
    
    * Create app.py
    
    * Update README.md
    
    * Update app.py
    
    * Update README.md
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
+
 # bandmates
 Add in or remove your favorite bandmates from songs
+
+![image](https://github.com/stekole/bandmates/assets/30674956/4b9942e2-7f7f-4d21-94d0-329309348993)
+
+# Introduction
+
+Ever hate your bassist in your band? Use this script to download audio and remove instruments on the fly using [demuc](https://github.com/facebookresearch/demucs) and [yt-dlp](https://github.com/yt-dlp/yt-dlp).
+
+Remove any of the following supported instruments in the model
+- bass
+- vocal
+- guitar
+- piano
+- drums
+
+## Pre-req
+
+```bash
+brew install python3 pyenv ffmpeg
+```
+
+```python
+pyenv install 3.10
+python3 -m venv env
+source env/bin/activate
+python3 -m pip install -r requirements.txt
+```
+
+## Usage
+
+karaoke fridays at the officeeee
+```python
+python3 app.py --link "https://www.youtube.com/watch?v=dQw4w9WgXcQ" -r vocals
+```
+
+Download a default of a guitar-less track from a link
+```python
+python3 app.py --link "https://www.youtube.com/watch?v=tMDFv5m18Pw&ab_channel=OzzyOsbourneVEVO"
+```
+
+Remove more than one instrument
+```python
+python3 app.py --link "https://www.youtube.com/watch?v=tMDFv5m18Pw&ab_channel=OzzyOsbourneVEVO" -r guitar -r vocals
+```
+## Input/Originals
+
+Original songs are kept in the `originals/` directory incase needed for reference later.
+
+## Output
+
+Output will end up in the `final/` folder in the format: `songName_<all_omitted_instruments_separated_by_underscores>_final.mp3`
+
+```example: ./final/Ozzy_Osbourne_-_Mama_I_m_Coming_Home_A440_Standard_Tuning_guitar_vocals_final.mp3```
+
---
+++ app.py
@@ -0,0 +1,110 @@
+from __future__ import unicode_literals
+import demucs.separate
+import os
+import shutil
+import sys
+import json
+import yt_dlp
+import click
+
+
+@click.command()
+@click.option('--link', help='link of the youtubezzz')
+@click.option('-r','--remove', help='what instrument do you want to strip? (example: `--remove guitar` or `-r bass -r drums -r vocals -r piano`)', default=["guitar"], multiple=True)
+
+
+def main(link,remove):
+
+    dir = 'original'
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    finishdir = 'final'
+    if not os.path.exists(finishdir):
+        os.mkdir(finishdir)
+
+    print("Your link is:  "+link)
+
+    songFileName = downloadSong(link,dir) #songFileName="Nirvana_-_The_Man_Who_Sold_The_World_MTV_Unplugged.mp3"
+    print(songFileName)
+
+    songfolderName = get_file_name(songFileName)
+    print("Original Download: "+songFileName)
+    processedFolder="./final/htdemucs_6s/"+songfolderName
+    print("Processed folder: "+processedFolder)
+
+    count=0
+    finishedIInstrumeent=[]
+    instumentToRemove = list(remove)
+    print("..... .... ... .. ... . ... ... .. .. . .. .")
+    for index,item in enumerate(instumentToRemove):
+        if index == 0 and item not in finishedIInstrumeent:
+            print("INPUT: "+songFileName)
+            finishSongPath=stripInstrumentFromSong(songFileName,item)
+            finishedIInstrumeent.append(item)
+            prev=index
+        else:
+            print(prev, instumentToRemove[prev])
+            inputsong=processedFolder+'/no_'+instumentToRemove[prev]+'.mp3' #input previous file
+            print("INPUT: "+inputsong)
+            finishSongPath=stripInstrumentFromSong(inputsong,item)
+            finishedIInstrumeent.append(item)
+            prev=index
+
+    print("This is the final song before rename: "+finishSongPath)
+    songFileNameNoExtention=os.path.splitext(os.path.basename(songFileName))[0]
+    fileExtention=os.path.splitext(os.path.basename(songFileName))[1]
+    print(songFileNameNoExtention)
+    finalSong='_'.join(instumentToRemove)
+    fileName="./"+finishdir+"/"+songFileNameNoExtention+"_"+finalSong+"_final.mp3"
+    print("After move: ./"+fileName)
+    shutil.copyfile('./'+finishSongPath, './'+fileName)
+
+
+def stripInstrumentFromSong(fileName,type):
+
+    demucs.separate.main(["-o","./final/","--mp3", "--two-stems", type, "-n", "htdemucs_6s", fileName])
+    # get foldername
+    file_path = fileName
+    folderName = get_file_name(file_path)
+    finishSongPath="./final/htdemucs_6s/"+folderName+"/no_"+type+".mp3"
+    print("Finished path:"+finishSongPath)
+    return finishSongPath
+
+def downloadSong(link,dir):
+    AUDIO_EXTENSION = 'mp3'
+    ydl_opts = {
+        'format': 'm4a/bestaudio/best',
+        'restrictfilenames': True,
+        # See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+        'postprocessors': [{  # Extract audio using ffmpeg
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': AUDIO_EXTENSION,
+        }],
+        'outtmpl':dir + '/%(title)s.%(ext)s',
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(link, download=True)
+        info_with_audio_extension = dict(info)
+        # Return filename with the correct extension
+        info_with_audio_extension['ext'] = AUDIO_EXTENSION
+
+        for k in info_with_audio_extension.keys():
+            if k == 'requested_downloads':
+                #print("Requested downloads: ")
+                requested_downloads=info_with_audio_extension.get("requested_downloads")
+                #print(requested_downloads)
+                for f in requested_downloads:
+                    filename=f.get("filepath")
+    return filename
+
+
+def get_file_name(file_path):
+    file_path_components = file_path.split('/')
+    file_name_and_extension = file_path_components[-1].rsplit('.', 1)
+    return file_name_and_extension[0]
+
+
+
+if __name__ == '__main__':
+    main()
